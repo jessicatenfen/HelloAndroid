7787b53e374cb75a41c604acc6b335436f0d761e
Jessica Tenfen <jessicatenfen@gmail.com>
2022-11-16 14:08:32 -0300

    alteração comentario versão 1.1

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.helloandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//comentário de alteração versao 1.0
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
